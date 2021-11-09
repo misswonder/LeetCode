783e7d14981e16c3010c8309820f608ffca85b1b
Yvonne Chen <yvonnechen1991@gmail.com>
2021-11-09 11:58:58 -0600

    Time: 96 ms (65.69%), Space: 48.6 MB (63.80%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -2,51 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var maxSubArray = function(nums) {
-    // let sum = 0;
-    // let max = nums[0];
-    // for (let i=0; i < nums.length; i++) {
-    //     for (let j=i+1; j <nums.length-1; j++) {
-    //         if (nums[i] + nums[j] > max) {
-    //             max = nums[i] + nums[j];
-    //         }
-    //     }
-    //     sum = max;
-    // }
-    // return sum;
-    
-//     let max = nums[0];
-    
-//     for(let i = 0; i < nums.length; i++) { 
-//         for(let j = i+1; j < nums.length + 1; j++) {
-//             let sum = 0;
-//             for(let k = i; k < j; k++) {
-//                 sum += nums[k];
-//             }
-//             if (sum > max) {
-//                 max = sum;
-//             }
-//         }
-//     }
-    
-//     return max;
-    
-//     let sums = [0];
-//     for (let i = 0; i < nums.length; i++) {
-//         sums[i+1] = sums[i] + nums[i];
-//     }
-    
-//     let max = nums[0];
-    
-//     for(let i = 0; i < nums.length; i++) { 
-//         for(let j = i+1; j < nums.length + 1; j++) {
-//             let sum = sums[j] - sums[i];
-//             if (sum > max) {
-//                 max = sum;
-//             }
-//         }
-//     }
-//     return max;
     
     let max = nums[0];
     let sum = nums[0];
