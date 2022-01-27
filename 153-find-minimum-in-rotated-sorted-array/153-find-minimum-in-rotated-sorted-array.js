121bc1039e650db804730e72f4ff3abd16db097e
Yvonne Chen <yvonnechen1991@gmail.com>
2022-01-27 02:11:46 -0600

    Time: 110 ms (27.98%), Space: 42.4 MB (5.05%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -3,19 +3,25 @@
  * @return {number}
  */
  function findMin(nums) {
-//     let left = 0;
-//     let right = nums.length - 1; 
+    let result = nums[0];
+    let left = 0;
+    let right = nums.length - 1; 
     
-//      while (left <= right) {
-//          let middle = (left + right) / 2;
-//          if (nums[middle] >= left) {
-//              left = middle + 1;
-//          } else {
-//              right = middle - 1;
-//          }
-//      }     
-//      return nums[left];
+     while (left <= right) {
+         if (nums[left] < nums[right]) {
+             result = Math.min(result, nums[left]);
+             break;
+         }
+         let middle = Math.floor((left + right) / 2);
+         result = Math.min(result, nums[middle]);
+         if (nums[middle] >= nums[left]) {
+             left = middle + 1;
+         } else {
+             right = middle - 1;
+         }
+     }     
+     return result;
      
-     let newNums = nums.sort((a , b) => (a- b));
-     return newNums[0];
+     // let newNums = nums.sort((a , b) => (a- b));
+     // return newNums[0];
 };
