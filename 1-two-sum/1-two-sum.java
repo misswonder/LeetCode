49925b37a6944c40199d43301f6e2748118f3854
Yvonne Chen <yvonnechen1991@gmail.com>
2022-02-03 17:42:48 -0600

    Time: 1 ms (99.77%), Space: 42.1 MB (39.91%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,40 +1,40 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] ans = new int[2];
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int[] ans = new int[2];
         
-        for(int i = 0; i<nums.length; i++){
-            if(map.containsKey(target - nums[i])){
-                ans[0] = map.get(target - nums[i]);
-                ans[1] = i;
-                return ans;
-            }
-            
-             map.put(nums[i], i);
-        }
-        
-        return null;
-    }
-}
-
-
-// public class Solution {
-//     public int[] twoSum(int[] numbers, int target) {
-        
-//         HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
-//         for(int i = 0; i < numbers.length; i++){
-
-//             Integer diff = (Integer)(target - numbers[i]);
-//             if(hash.containsKey(diff)){
-//                 int toReturn[] = {hash.get(diff)+1, i+1};
-//                 return toReturn;
+//         for(int i = 0; i<nums.length; i++){
+//             if(map.containsKey(target - nums[i])){
+//                 ans[0] = map.get(target - nums[i]);
+//                 ans[1] = i;
+//                 return ans;
 //             }
-
-//             hash.put(numbers[i], i);
-
+            
+//              map.put(nums[i], i);
 //         }
         
 //         return null;
-        
 //     }
 // }
+
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
+        for(int i = 0; i < nums.length; i++){
+
+            Integer diff = (Integer)(target - nums[i]);
+            if(hash.containsKey(diff)){
+                int toReturn[] = {hash.get(diff), i};
+                return toReturn;
+            }
+
+            hash.put(nums[i], i);
+
+        }
+        
+        return null;
+        
+    }
+}
