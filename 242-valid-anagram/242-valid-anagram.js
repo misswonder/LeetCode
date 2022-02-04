426a53124e0a06e423b31e645cea10f9bfa778c2
Yvonne Chen <yvonnechen1991@gmail.com>
2022-02-03 23:25:25 -0600

    Time: 84 ms (92.05%), Space: 49.7 MB (5.37%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,32 +4,32 @@
  * @return {boolean}
  */
 // time O(n) space O(n)
-var isAnagram = function(s, t) {
-    if(s.length !== t.length) {
-        return false
-    }
-    
-    const hashMap = new Map()
-    
-    for(const letter of s) {
-        hashMap.set(letter, hashMap.get(letter) + 1 || 1)
-    }
-    
-    for(const letter of t) {
-        if(!hashMap.get(letter)) {
-            return false
-        }
-        hashMap.set(letter, hashMap.get(letter) - 1)
-    }
-    
-    return true
-};
-
-// time O(n log n) space O(n)
 // var isAnagram = function(s, t) {
 //     if(s.length !== t.length) {
 //         return false
 //     }
     
-//     return s.split('').sort().join('') === t.split('').sort().join('')
+//     const hashMap = new Map()
+    
+//     for(const letter of s) {
+//         hashMap.set(letter, hashMap.get(letter) + 1 || 1)
+//     }
+    
+//     for(const letter of t) {
+//         if(!hashMap.get(letter)) {
+//             return false
+//         }
+//         hashMap.set(letter, hashMap.get(letter) - 1)
+//     }
+    
+//     return true
 // };
+
+// time O(n log n) space O(n)
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) {
+        return false
+    }
+    
+    return s.split('').sort().join('') === t.split('').sort().join('')
+};
