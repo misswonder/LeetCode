c648843e29d81ccf9629e39e734b0dd93257605b
Yvonne Chen <yvonnechen1991@gmail.com>
2022-02-03 23:24:57 -0600

    Time: 156 ms (24.17%), Space: 44.5 MB (26.83%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -3,6 +3,33 @@
  * @param {string} t
  * @return {boolean}
  */
+// time O(n) space O(n)
 var isAnagram = function(s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('');
+    if(s.length !== t.length) {
+        return false
+    }
+    
+    const hashMap = new Map()
+    
+    for(const letter of s) {
+        hashMap.set(letter, hashMap.get(letter) + 1 || 1)
+    }
+    
+    for(const letter of t) {
+        if(!hashMap.get(letter)) {
+            return false
+        }
+        hashMap.set(letter, hashMap.get(letter) - 1)
+    }
+    
+    return true
 };
+
+// time O(n log n) space O(n)
+// var isAnagram = function(s, t) {
+//     if(s.length !== t.length) {
+//         return false
+//     }
+    
+//     return s.split('').sort().join('') === t.split('').sort().join('')
+// };
