4adec52e160e2aefac574c6de7ba53bb1a6fafb0
Yvonne Chen <yvonnechen1991@gmail.com>
2022-02-23 15:12:34 -0600

    Time: 98 ms (51.56%), Space: 43 MB (17.67%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    
+    let l = 0;
+    let r = numbers.length - 1;
+    
+    while (l < r) {
+        let currSum = numbers[l] + numbers[r];
+        
+        if (currSum > target) {
+            r -= 1;
+        } else if (currSum < target) {
+            l += 1;
+        } else {
+            return [l+1, r+1];
+        }      
+    }
+    
+    return [];
+};
