c4b6c82aabf2b94a8253fab3425820139e29ca01
Yvonne Chen <yvonnechen1991@gmail.com>
2022-06-21 22:19:06 -0500

    Time: 111 ms (56.97%), Space: 43.2 MB (28.55%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -7,12 +7,13 @@ var twoSum = function(nums, target) {
     
     let map = new Map();
     
-    for(i=0; i<nums.length; i++) {
+    for (i=0; i<nums.length; i++) {
         if(map.has(target - nums[i])) {
             return [map.get(target - nums[i]), i]
         } else {
             map.set(nums[i] , i)
         }
     }
+    
     return [];
 };
