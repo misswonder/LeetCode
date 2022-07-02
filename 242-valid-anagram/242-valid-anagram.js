dd12ef49b3bd07b0a77a0747285d733499b0fe20
Yvonne Chen <yvonnechen1991@gmail.com>
2022-07-02 16:43:49 -0500

    Time: 104 ms (71.05%), Space: 49.4 MB (8.11%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -6,10 +6,12 @@
 var isAnagram = function(s, t) {
     
     if (s.length !== t.length) {
-        return false; 
+        return false;
     }
     
+    const s1 = s.split("").sort().join("");
+    const t1 = t.split("").sort().join("");
     
-    return s.split("").sort().join("") === t.split("").sort().join("");
+    return s1 === t1;
     
 };
