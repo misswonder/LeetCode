d15cd34a803eaf72dea597f53700025df4af5162
Yvonne Chen <yvonnechen1991@gmail.com>
2022-07-03 01:23:11 -0500

    Time: 4 ms (74.15%), Space: 45.9 MB (30.30%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,14 +1,20 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
         
-        for (int i=0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                return new int[]{map.get(nums[i]), i};
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int diff = target - n;
+            
+            if(map.containsKey(n)) {
+                return new int[]{map.get(n), i};
+            } else {
+                map.put(diff, i);
             }
-            map.put(target - nums[i], i);
         }
-        
         return new int[0];
     }
 }
