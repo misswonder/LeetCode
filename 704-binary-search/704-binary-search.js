0f8d0de51de8a82a693b3cc12f404ab42d119af4
Yvonne Chen <yvonnechen1991@gmail.com>
2022-07-19 14:56:22 -0500

    Time: 64 ms (96.31%), Space: 45.6 MB (9.95%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -5,9 +5,17 @@
  */
 var search = function(nums, target) {
     
-    for (i=0; i < nums.length; i ++) {
-        if (nums[i] === target) {
-            return i;
+    let l = 0;
+    let r = nums.length - 1;
+    
+    while(l <= r) {
+        let m = Math.floor((l + r) / 2);
+        if(nums[m] < target) {
+            l = m + 1;
+        } else if(nums[m] > target) {
+                r = m - 1;          
+        } else {
+            return m;
         }
     }
     
