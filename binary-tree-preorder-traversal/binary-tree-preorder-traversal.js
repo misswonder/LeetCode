d35d20f5e955d86e3903c6cc2c410c09a0c7abe8
Yvonne Chen <yvonnechen1991@gmail.com>
2022-07-27 17:24:02 -0500

    Time: 60 ms, Memory: 42.3 MB - LeetHub

---
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    
+    if(!root) return [];
+    if(root.size === 1) return root.val;
+    
+    let data = [];
+    const traverse = (node) => {
+        data.push(node.val);
+        if(node.left) traverse(node.left);
+        if(node.right) traverse(node.right);
+    }
+    
+    traverse(root);
+    return data;
+};
