84667d24dd0272f6bca76e6cae02331edaefe9e6
Yvonne Chen <yvonnechen1991@gmail.com>
2022-09-15 16:06:59 -0500

    Time: 114 ms (50.46%), Space: 44.5 MB (71.80%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -2,27 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
-
 var isPalindrome = function(s) {
-  if (s.length === 0) {
-    return true;
-  }
-
-  // const regex = /[\_\W]/g;
-    const regex = /[^a-z0-9$]/gi;
-  const cleaned = [...s.toLowerCase().replace(regex, "")];
-
-  let i = 0;
-  let j = cleaned.length - 1;
-
-  while (i < j) {
-    if (cleaned[i] !== cleaned[j]) {
-      return false;
+    
+    if (s.length === 0) {
+        return true;
     }
-
-    i++;
-    j--;
-  }
-
-  return true;
+    
+    const s1 = s.replace(/[^a-z0-9]/ig, "").toLowerCase();
+    
+    let l = 0;
+    let r = s1.length - 1;
+    
+    while (l < r) {
+        
+        if (s1[l] !== s1[r]) {
+            return false;
+        }
+        
+        l++;
+        r--;
+    }
+    
+    return true;
+    
 };
