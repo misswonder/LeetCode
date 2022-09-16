e7a4b0edf64d2c3b5a0a6f6291b784ce05749d57
Yvonne Chen <yvonnechen1991@gmail.com>
2022-09-16 12:05:52 -0500

    Time: 92 ms (90.40%), Space: 52.1 MB (16.30%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -4,10 +4,10 @@
  */
 var maxProfit = function(prices) {
     
-    // let [left, right, max] = [0, 1, 0];
-    let left = 0;
-    let right = 1;
-    let max = 0;
+    let [left, right, max] = [0, 1, 0];
+//     let left = 0;
+//     let right = 1;
+//     let max = 0;
     
     while (right < prices.length) {
         
