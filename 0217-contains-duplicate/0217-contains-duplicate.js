a0af3b431e9c730e22457f58a99bdb691dc6ab96
Yvonne Chen <yvonnechen1991@gmail.com>
2023-02-02 12:20:36 -0600

    Time: 87 ms (86.74%), Space: 55 MB (14.35%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,9 +2,13 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+// Time 0(N), Space O(N)
+
 var containsDuplicate = function(nums) {
     
     const numsSet = new Set(nums);
     return numsSet.size !== nums.length; 
     
 };
+
