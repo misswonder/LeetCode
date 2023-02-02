990472515849c370a96f9b2d27351e7f5add4f43
Yvonne Chen <yvonnechen1991@gmail.com>
2023-02-02 15:49:10 -0600

    Time: 75 ms (80.81%), Space: 44.2 MB (80.56%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -7,43 +7,23 @@
 
 var isPalindrome = function(s) {
     
-//     if (s.length === 0) {
-//         return true;
-//     }
-    
-//     const s1 = s.replace(/[^a-z0-9)]/ig, "").toLowerCase();
-
-//     let l = 0;
-//     let r = s1.length - 1;
-    
-//     while (l < r) {
-//         if (s1[l] !== s1[r]) {
-//             return false;
-//         } 
-//             l++;
-//             r--;
-//     }
-    
-//      return true;
-    
     if (s.length === 0) {
         return true;
     }
     
     const s1 = s.replace(/[^a-z0-9]/ig, "").toLowerCase();
-    
+
     let l = 0;
     let r = s1.length - 1;
     
     while (l < r) {
-        
         if (s1[l] !== s1[r]) {
             return false;
-        }
-        
-        l++;
-        r--;
+        } 
+            l++;
+            r--;
     }
     
-    return true;
+     return true;
+
 };
