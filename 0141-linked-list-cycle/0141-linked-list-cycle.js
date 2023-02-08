9e3262620b8369741623eff5a8838f3e06262752
Yvonne Chen <yvonnechen1991@gmail.com>
2023-02-08 14:25:07 -0600

    Time: 75 ms (82.39%), Space: 44.7 MB (62.19%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+// Time O(N) | Space 0(1)
+
+var hasCycle = function(head) {
+    
+    let slow = head;
+    let fast = head; 
+    
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        
+        if (slow === fast) {
+            return true;
+        }
+    }
+    
+    return false; 
+    
+};
