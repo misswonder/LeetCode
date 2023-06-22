8e15a3b495e88a60ed5597d5225663ad069f3ad8
Yvonne Chen <yvonnechen1991@gmail.com>
2023-06-22 13:05:28 -0500

    Time: 70 ms (6.46%), Space: 42 MB (42.54%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    
+// declarative programming
+    // return arr.filter(fn);
+    
+    // imperative programming
+    const res = [];
+     for (let i=0; i < arr.length; i++) {
+         if(fn(arr[i], i)) {
+             res.push(arr[i]);
+         }
+     }
+    return res;
+};
