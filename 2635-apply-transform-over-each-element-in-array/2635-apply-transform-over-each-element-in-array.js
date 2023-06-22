d647be5a4ded35db29a683e08283025b18900964
Yvonne Chen <yvonnechen1991@gmail.com>
2023-06-22 13:28:46 -0500

    Time: 58 ms (60.65%), Space: 42.1 MB (35.31%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -6,9 +6,6 @@
 var map = function(arr, fn) {
     // return arr.map(fn);
     
-    // for (cost i in arr) => i = index
-    // for (cost n of arr) => n = value
-    
    const res = [];
     
     for (const i in arr) {
