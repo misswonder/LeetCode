4c876232c5bef443a5302f5e297f7eff87ce0254
Yvonne Chen <yvonnechen1991@gmail.com>
2023-06-22 15:07:21 -0500

    Time: 52 ms (93.73%), Space: 42.6 MB (45.21%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    
+//     const n = [1, 2, 7, 4, 5];
+//     function fn(init, n) {
+//         return init * n;
+//     }
+    
+//     console.log(nums.reduce(fn, 0));
+    
+    return nums.reduce(fn, init);
+    
+    // let res = 0;
+    // for (let i=0; i < nums.length; i++) {
+    //     res += (fn, nums[i]);
+    // }
+    // return res;
+    
+};
