20769af210686de9978b3390a91cb76158c3db54
Yvonne Chen <yvonnechen1991@gmail.com>
2023-06-22 15:14:39 -0500

    Time: 78 ms (5.04%), Space: 43.9 MB (19.83%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -15,17 +15,17 @@ var reduce = function(nums, fn, init) {
     
     // return nums.reduce(fn, init);
     
+    // let res = init;
+    // for (let i=0; i < nums.length; i++) {
+    //     res = fn(res, nums[i]);
+    // }
+    // return res;
+    
     let res = init;
-    for (let i=0; i < nums.length; i++) {
-        res = fn(res, nums[i]);
+    for (const n of nums) {
+        res = fn(res, n)
     }
+    
     return res;
-    
-//     let res = init;
-//     for (const n of nums) {
-//         res = fn(res, n)
-//     }
-    
-//     return res;
     
 };
